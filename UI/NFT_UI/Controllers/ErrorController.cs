7d967d9f8e9f4dce3465cd8d1500677a1c2045da
umerkhayam3 <ukhayam211@gmail.com>
2024-11-07 21:20:13 +0500

    ...

---
+++ UI/NFT_UI/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiVerse_UI.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ErrorController(IHttpContextAccessor httpContextAccessor)
+        {
+            this._httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
